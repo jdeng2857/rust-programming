823112fe1ec4679990bf33c1b28b7408165ecf0b
jdeng2857 <jdeng2857@gmail.com>
2023-05-17 08:45:09 -0400

    Add basic box implementation

---
+++ 15_Smart_Pointers/box_t/src/main.rs
@@ -0,0 +1,17 @@
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
+fn box_basics() {
+    let b = Box::new(5);
+    println!("b = {b}");
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+}
+
+fn main() {
+    box_basics();
+}
